623ea21d8a98d1ad6a591ce993576b573effc1c5
siddhantbhardwaj <bhardwajsiddhant6@gmail.com>
2025-04-20 15:32:31 +0530

    Adding test main files

---
+++ DSA/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        List<Integer> list = new ArrayList<>();
+//        int[] input = {2,5,4};
+//
+//        Stack<Integer> stack = new Stack<>();
+//        Integer largest = input[0];
+//        Integer SecondLargest = Integer.MIN_VALUE;
+//        for(int i: input){
+//            if(largest < i) {
+//                SecondLargest = largest;
+//                largest = i;
+//            }
+//        }
+//
+//        System.out.println(SecondLargest);
+
+        int[] array = new int[5];
+        System.out.println(Arrays.toString(array));
+    }
+}
+
+// Second largest element in array
+// [1,2,4]
+// Sort the array
---
+++ PractiseCode/src/Main.java
@@ -0,0 +1,13 @@
+package src;
+
+import src.equalfunctionality.Dog;
+
+import java.util.Arrays;
+
+public class Main extends Dog {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        System.out.println(Arrays.toString(array));
+    }
+
+}
