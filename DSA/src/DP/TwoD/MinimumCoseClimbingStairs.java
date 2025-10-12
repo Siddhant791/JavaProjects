8330e826b94434825a5a188c1b3f5e7521f68d6a
siddhantbhardwaj <bhardwajsiddhant6@gmail.com>
2025-10-13 02:18:38 +0530

    Add optimized method to MinimumCoseClimbingStairs class: implement space-efficient solution for minimum cost calculation

--- DSA/src/DP/TwoD/MinimumCoseClimbingStairs.java
+++ DSA/src/DP/TwoD/MinimumCoseClimbingStairs.java
@@ -57,4 +57,11 @@ public class MinimumCoseClimbingStairs {
         dp[i] = cost[i] + Math.min(first,second); // current cost plus cheaper of the two options
         return dp[i]; // return and memoize result
     }
+
+    public int minCostClimbingStairsMostOptimized(int[] cost){
+        for (int i = cost.length - 3 ; i>=0; i--){
+            cost[i] += Math.min(cost[i+1], cost[i+2]);
+        }
+        return Math.min(cost[0], cost[1]);
+    }
 }
