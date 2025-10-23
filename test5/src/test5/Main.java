40749e57366de63ed11161675562314a1c780ba7
siddhantbhardwaj <bhardwajsiddhant6@gmail.com>
2025-10-23 15:50:57 +0530

    Second commit

--- test5/src/test5/Main.java
+++ test5/src/test5/Main.java
@@ -11,8 +11,7 @@ public class Main {
 	
 	public static void main(String[] args) throws UserDefinedExeption {
 		Scanner sc = new Scanner(System.in);
-        //harish
-		int test = 0;
+		int siddhant = 0;
 		while(true) {
 			System.out.println("Choose from given below menu \n 1.) ArrayList \n 2.) LinkedHashSet \n 3.)TreeSet \n 4.) Exit ");
 			input = sc.nextInt();sc.nextLine();
