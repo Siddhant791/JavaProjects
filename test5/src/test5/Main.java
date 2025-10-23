d88795040cdf6fca9dc94dc7a5ab2367bea48f94
Siddhant Bhardwaj <67505660+Siddhant791@users.noreply.github.com>
2025-10-23 16:10:18 +0530

    Update Main.java

--- test5/src/test5/Main.java
+++ test5/src/test5/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	
 	public static void main(String[] args) throws UserDefinedExeption {
 		Scanner sc = new Scanner(System.in);
-		int siddhant = 0;
+		int tanya = 0;
 		while(true) {
 			System.out.println("Choose from given below menu \n 1.) ArrayList \n 2.) LinkedHashSet \n 3.)TreeSet \n 4.) Exit ");
 			input = sc.nextInt();sc.nextLine();
@@ -54,4 +54,5 @@ class UserDefinedExeption extends Exception{
 		super(s);
 	}
 }
+
 
